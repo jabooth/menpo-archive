d812f4b65e7a6df2be7c9697479ef27f6a80d67e
James Booth <jabooth@gmail.com>
2013-04-29 19:04:39 +0100

    split const into constfunction/constmethod

--- pybug/decorator/__init__.py
+++ pybug/decorator/__init__.py
@@ -2,9 +2,9 @@ from decorator import decorator
 
 
 @decorator
-def const(f, *args, **kwargs):
+def constfunction(f, *args, **kwargs):
     """
-    Decorator to maintain constness of parameters.
+    Decorator to maintain constness of parameters for a function.
 
     This should be used on any function that takes ndarrays as parameters.
     This helps protect against potential subtle bugs caused by the pass by
@@ -20,6 +20,41 @@ def const(f, *args, **kwargs):
     """
     copy = kwargs.get('copy', True)
     if copy:
-        args = [x.copy() for x in args]
-        kwargs = dict((k, v.copy()) for k, v in kwargs.items())
+        args, kwargs = _const_helper(args, kwargs)
     return f(*args, **kwargs)
+
+
+@decorator
+def constmethod(method, *args, **kwargs):
+    """
+    Decorator to maintain constness of parameters for a class method.
+
+    This should be used on any method that takes ndarrays as parameters.
+    This helps protect against potential subtle bugs caused by the pass by
+    reference nature of python arguments. Copy is assumed to a shallow copy.
+
+    :param method: class method to wrap
+    :param args: method's args
+    :param kwargs: method's kwargs
+                   copy {True, False}
+                       If False then don't copy the parameters - use with
+                       caution
+    """
+    copy = kwargs.get('copy', True)
+    if copy:
+        self = args[0]
+        print 'self is: ' + str(self)
+        args, kwargs = _const_helper(args[1:], kwargs)
+        args = self + args
+    return method(*args, **kwargs)
+
+
+def _const_helper(*args, **kwargs):
+    """
+    Returns a copy of the args and kwargs passed in.
+    """
+    print 'args are: ' + str(args)
+    print 'kwargs are: ' + str(kwargs)
+    args = [x.copy() for x in args]
+    kwargs = dict((k, v.copy()) for k, v in kwargs.items())
+    return args, kwargs
